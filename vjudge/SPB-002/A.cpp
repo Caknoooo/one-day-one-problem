bfd070ff16573f1a6e9d06746db8dab416c071ba
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-04-20 19:35:55 +0700

    feat: add problem solution spb-002

---
+++ vjudge/SPB-002/A.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define psi pair<string, int>
+#define pis pair<int, string>
+#define pll pair<long long, long long>
+#define all(x) x.begin(),x.end()
+#define revAll(x) x.rbegin(),x.rend()
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define loop(t) int t; cin >> t; while(t--)
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define seev(v,n) for(int i = 0; i < n; i++){int x; cin >> x; v.push_back(x);}
+#define sees(s,n) for(int i = 0; i < n; i++){int x; cin >> x; s.insert(x);}
+#define MAX 10005
+#define MOD 1000000007
+typedef long long ll;
+ll ct = 0;
+
+void solve() {
+  vector<int> v;
+  seev(v, 7);
+  sort(all(v));
+
+  cout << v[6] - v[0] - v[1] << ' ' << v[0] << ' ' << v[1] << '\n'; 
+}
+
+int main() {
+  int tc;
+  cin >> tc;
+  while(tc-- > 0) solve();
+  return 0;
+}
